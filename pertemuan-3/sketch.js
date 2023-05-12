db1f022cef5439a670aded31c8d7e8e6cd95313c
OnedaO2 <104665607+OnedaO2@users.noreply.github.com>
2023-05-12 17:24:44 +0700

    Tugas3_21104410031

---
+++ pertemuan-3/sketch.js
@@ -0,0 +1,109 @@
+//Praktik 1
+function setup() {
+  createCanvas(200, 200);
+}
+function draw() {
+  background(0);
+}
+
+//Praktik 2
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(252, 61, 224);
+}
+
+//Praktik 3
+function setup() {
+  createCanvas(200, 200);
+}
+function draw() {
+  background(220);
+  fill(255, 255, 0);
+  circle(100, 100, 50);
+}
+
+//Praktik 4
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(220);
+  fill(255, 255, 0);
+  circle(100, 100, 50);
+  rect(0, 0, 100, 50);
+  ellipse(50, 70, 30, 30);
+}
+
+//Praktik 5
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(220);
+  fill(255, 255, 0);
+  circle(100, 100, 50);
+  rect(0, 0, 100, 50);
+  noFill();
+  ellipse(50, 70, 30, 30);
+}
+
+//Praktik 6
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(220);
+  fill(255, 255, 0, 50);
+  circle(100, 100, 50);
+  rect(0, 0, 100, 50);
+  noFill();
+  ellipse(50, 70, 30, 30);
+}
+
+//Praktik 7
+function setup() {
+  createCanvas(200, 200);
+}
+function draw() {
+  background(220);
+  fill(255, 255, 0, 50);
+  // Garis Hijau
+  stroke(0, 255, 0);
+  circle(100, 100, 50);
+  // Garis Merah
+  stroke(255, 0, 0);
+  rect(0, 0, 100, 50);
+  noFill();
+  // Garis Biru
+  stroke(0, 0, 255);
+  ellipse(50, 70, 30, 30);
+}
+
+//No Smoke
+function setup() {
+  createCanvas(200, 200);
+}
+function draw() {
+  background(220);
+  fill(255, 255, 0, 50);
+  // Garis Hijau
+  stroke(0, 255, 0);
+  circle(100, 100, 50);
+  // Garis Merah
+  stroke(255, 0, 0);
+  rect(0, 0, 100, 50);
+  noFill();
+ // Garis Biru
+ stroke(0, 0, 255);
+ ellipse(50, 70, 30, 30);
+ // Tanpa Garis
+ noStroke();
+ fill(255, 255, 0);
+ circle(20, 180, 50);
+} 
